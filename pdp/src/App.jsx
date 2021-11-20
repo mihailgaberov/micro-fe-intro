fe54944a12ccfe4b02d8465c1009c0526272bbdb
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-11-20 14:08:18 +0100

    :sparkles: Added PDPContent.jsx

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -4,11 +4,12 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import PDPContent from "./PDPContent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header />
-    <div className="my-10">PDP Page Content</div>
+    <div className="my-10"><PDPContent /></div>
     <Footer />
   </div>
 );
