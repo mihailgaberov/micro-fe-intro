3f2b88643f8f65a6f5d4f631bf5bc7fe4ff124fc
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-11-21 12:20:45 +0100

    :sparkles: Added main methods for manipulating the cart.

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,9 +1,53 @@
 import { BehaviorSubject } from 'rxjs';
 import { useEffect, useState } from 'react';
+import { fetch } from "rxjs/src/internal/umd";
 
 const API_SERVER = "http://localhost:8080";
 
 export const jwt = new BehaviorSubject(null);
+export const cart = new BehaviorSubject(null);
+
+export const clearCart = () =>
+  fetch(`${API_SERVER}/cart`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`
+    },
+    body: JSON.stringify({ id })
+  })
+    .then(res => res.json())
+    .then(() => {
+      getCart();
+    });
+
+export const addCart = (id) =>
+  fetch(`${API_SERVER}/cart`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`
+    },
+    body: JSON.stringify({ id })
+  })
+    .then(res => res.json())
+    .then(() => {
+      getCart();
+    });
+
+export const getCart = () =>
+  fetch(`${API_SERVER}/cart`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`
+    }
+  })
+    .then(res => res.json())
+    .then(res => {
+      cart.next(res);
+      return res;
+    });
+
 
 export const login = (username, password) =>
   fetch(`${API_SERVER}/auth/login`, {
@@ -18,6 +62,7 @@ export const login = (username, password) =>
   }).then(res => res.json())
     .then(data => {
       jwt.next(data.access_token);
+      getCart();
       return data.access_token;
     });
 
@@ -27,7 +72,7 @@ export function useLoggedIn() {
   useEffect(() => {
     setLoggedIn(!!jwt.value);
 
-    return jwt.subscribe(c => setLoggedIn(!!jwt.value));
+    return jwt.subscribe(() => setLoggedIn(!!jwt.value));
   }, []);
 
   return loggedIn;
