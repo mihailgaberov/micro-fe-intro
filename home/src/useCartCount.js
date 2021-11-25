157cda017d547c4299c60a6bc906b39b9bc267a1
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-11-25 09:46:04 +0100

    :white_check_mark: Added unit tests.

---
+++ home/src/useCartCount.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+import {cart} from "cart/cart";
+
+export function useCartCount() {
+  const [count, setCount ] = useState(cart.cartItems.length);
+
+  useEffect(() => {
+    cart.subscribe(({ cartItems }) => setCount(cartItems.length));
+  }, []);
+
+  return count;
+}
