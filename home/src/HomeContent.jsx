cdb0d8f6469234874996d15f88d02ea00a7d107d
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-11-25 09:28:05 +0100

    :sparkles: Added links on the home page to the product page.

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { getProducts, currency } from "./products";
 import { addToCart, useLoggedIn } from "cart/cart";
+import { Link } from "react-router-dom";
 
 export default function HomeContent() {
   const loggedIn = useLoggedIn();
@@ -14,10 +15,10 @@ export default function HomeContent() {
   return <div className="grid grid-cols-4 gap-5">
     {products.map(product => (
       <div key={product.id}>
-        <img src={product.image} alt={product.name} />
+        <Link to={`product/${product.id}`}><img src={product.image} alt={product.name} /></Link>
         <div className="flex">
           <div className="flex-grow font-bold">
-            <a>{product.name}</a>
+            <Link to={`product/${product.id}`}><a>{product.name}</a></Link>
           </div>
           <div className="flex-end">
             {currency.format(product.price)}
