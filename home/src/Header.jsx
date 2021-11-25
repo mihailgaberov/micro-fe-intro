360c063f132f26a8e1be32eaa17923910acfbdff
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-11-25 09:24:29 +0100

    :lipstick: Styling.

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -7,10 +7,10 @@ import MiniCart from "cart/MiniCart";
 export default function Header() {
   return <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
     <div className="flex">
-      <div className="flex-grow">
+      <div className="flex-grow flex">
         <Link to="/">Fidget Spinner World</Link>
-        |
-        <Link to="/cart">Cart</Link>
+        <div className="mx-5">|</div>
+        <Link id="cart" to="/cart">Cart</Link>
       </div>
       <div className="flex-end relative">
         <MiniCart />
